28a5b324592dce91b3f47165d41db2475b060a4b
Atoqaz <atoqaz@gmail.com>
2023-10-30 18:06:44 +0100

    Split melodies into seperate files

--- Tunes.py
+++ Tunes.py
@@ -10,11 +10,15 @@ import os
 
 DIR = Path(__file__).parent
 
-to_datapack = 1
-melody = "ghostbusters"
+to_datapack = 0
+melody = "amtet"
 saves_folder = "TestyTown"
 datapack_name = "groovy tunes"
 
+if melody == "ghostbusters":
+    from melodies.ghostbusters import *
+elif melody == "amtet":
+    from melodies.amtet import *
 
 if to_datapack:
     location = Path(os.getenv("APPDATA")).joinpath(
@@ -24,81 +28,6 @@ else:
     location = DIR.joinpath(
         f"{datapack_name}/data/tunes/functions/melody/{melody}.mcfunction"
     )  # Testing
-
-pling = ""
-harp = ""
-bell = ""
-chime = ""
-bit = ""
-xylophone = ""
-banjo = ""
-guitar = ""
-bass = ""
-didgeridoo = ""
-hat = ""
-snare = ""
-basedrum = ""
-
-if melody == "ghostbusters":
-
-    pling += "gc.gc.-dfA#-cfA-gc.gc.-dfA#-cfA-gc.gc.-dfA#-cfA-gc.gc.-dfA#.dfA#.cfA- "
-    pling += ".C,C,GCE.C.fA#D.A#.--C,C,C,C,dfA#.C.-.C,C,GCE.C.fA#D.A#.--C,C,C,C,dfA#.D.fAC.C,C, "
-    pling += "GD#.C.D#-dfA#-cfA.C,A#,GC.C.C-dfA#-cfA-gc.gc.-dfA#-cfA-gc.gc.-dfA#.dfA#.cfA.C,C, "
-    pling += "GD#.C.D#-dfA#-cfA.C,A#,GC.C.C-dfA#-cfA-gc.gc.-fA#D-fAC-gc.gc.-fA#D.fA#D.fAC.G. "
-    pling += "D#;C;D#;C;D#;C;D#;C;D#.C.A#,B,C.D#;C;D#;C;D#;C;D#;C;D#.C.A#,B,C. "
-    pling += "D#G;CD#;D#G;CD#;D#G;CD#;D#G;CD#;D#G.CD#.A#,B,C.D#G;CD#;D#G;CD#;D#G;CD#;D#G;CD#;D#G.CD#.A#,B,C. "
-    pling += "cGC.cGC."
-
-    bass += "c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f. "
-    bass += "c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f. "
-    bass += "c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f. "
-    bass += "c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.g. "
-    bass += "c-C-c-A#-a#-A#-a#A#.a#A#.a#A#.a#A#.a-A-a-f-f-f-f.f.f.f. "
-    bass += "c-C-c-A#-a#-A#-a#A#.a#A#.a#A#.a#A#.a-A-a-f-f-f-f.f.f.f. "
-    bass += "cC.cC."
-
-elif melody == "amtet":
-
-    pling += "&&"
-    pling += "B;A;f#-d....A,A,A.A.B.D.D.B."
-    pling += "E;D;B.A..f#.A.B;A;f#.e..e.d."
-    pling += "e;f#;d----.G;G;G."
-    pling += ".f#.-------"
-    pling += "&&&&"
-    pling += "B;A;f#-d....A,A,A.A.B.D.D.B."
-    pling += "E;D;B.A..f#.A.B;A;f#.e..e.d."
-    pling += "e;f#;d----.G;G;G."
-    pling += ".f#"
-
-    banjo += "&&&&&&&&&&"
-    banjo += "B;A;f#-d....A,A,A.A.B.D.D.B."
-    banjo += "E;D;B.A..f#.A.B;A;f#.e..e.d."
-    banjo += "e;f#;d----.G;G;G."
-    banjo += ".f#"
-
-    chime += "&&&&&&&&&&&&------G;G;G."
-    chime += ".f#"
-
-    xylophone += ".A,A,A.A,A.A,A.B-.A,A,A.A,A.A,A.G#-"
-    xylophone += ".A,A,A.A,A.A,A.B-.A,A,A.A,A.A,A.G#-"
-    xylophone += ".A,A,A.A,A.A,A.B-.A,A,A.A,A.A,A.G#-"
-    xylophone += ".A,A,A.A,A.A,A.B-.A,A,A.A,A.A,A.G#-"
-    xylophone += ".B,B,B.B,B.C#;B.A..E,E,E.E,E.F#;E.C#."
-    xylophone += ".A,A,A.A,A.A,A.B-.A,A,A.A,A.A,A.B-"
-
-    harp += "&&&&&&"
-    harp += "a-.a--c#.d-.d-.d-b-.b.--d-.c#.--"
-    harp += "a-.a--c#.d-.d-.d-b-.b.--d-.c#.--"
-
-    bass += "aaa----aaa----aaa----aaa----"
-    bass += "aaa----aaa---g#g#g#-#f#f#f-------ccc-bbb---bBB-g#G#G#----aaa----aaa----"
-    bass += "aaa-.aaa-eee.ggg.g#g#g#.aaa-.aaa-eee.ggg.g#g#g#.ddd-.ddd-aaa.ccc.ddd.eee;ccc;ddd-ggg.ccc.aaa."
-    bass += "aaa-.aaa-eee.ggg.g#g#g#.aaa-.aaa-eee.ggg.g#g#g#.ddd-.ddd-aaa.ccc.ddd.eee;ccc;ddd-ggg.ccc.aaa."
-    bass += "aaa----aaa---g#g#g#-#f#f#f-------ccc-bbb---bBB-g#G#G#----aaa"
-
-    hat += "-f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#---"
-    snare += "&&&&&-------f#,f#;-f#--f#--f#--f#--f#--f#--f#--f#,f#;-f#--f#--f#--f#--f#--f#--f#-.f#,f#,f#,f#;-f#--f#--f#--f#--f#--f#--f#--f#,f#;-f#--f#--f#--f#-"
-    basedrum += "&&&&&&f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--f#--"
 
 
 notes_instruments = [
---
+++ melodies/ghostbusters.py
@@ -0,0 +1,36 @@
+pling = ""
+harp = ""
+bell = ""
+chime = ""
+bit = ""
+xylophone = ""
+banjo = ""
+guitar = ""
+bass = ""
+didgeridoo = ""
+hat = ""
+snare = ""
+basedrum = ""
+
+
+pling += "gc.gc.-dfA#-cfA-gc.gc.-dfA#-cfA-gc.gc.-dfA#-cfA-gc.gc.-dfA#.dfA#.cfA- "
+pling += (
+    ".C,C,GCE.C.fA#D.A#.--C,C,C,C,dfA#.C.-.C,C,GCE.C.fA#D.A#.--C,C,C,C,dfA#.D.fAC.C,C, "
+)
+pling += (
+    "GD#.C.D#-dfA#-cfA.C,A#,GC.C.C-dfA#-cfA-gc.gc.-dfA#-cfA-gc.gc.-dfA#.dfA#.cfA.C,C, "
+)
+pling += (
+    "GD#.C.D#-dfA#-cfA.C,A#,GC.C.C-dfA#-cfA-gc.gc.-fA#D-fAC-gc.gc.-fA#D.fA#D.fAC.G. "
+)
+pling += "D#;C;D#;C;D#;C;D#;C;D#.C.A#,B,C.D#;C;D#;C;D#;C;D#;C;D#.C.A#,B,C. "
+pling += "D#G;CD#;D#G;CD#;D#G;CD#;D#G;CD#;D#G.CD#.A#,B,C.D#G;CD#;D#G;CD#;D#G;CD#;D#G;CD#;D#G.CD#.A#,B,C. "
+pling += "cGC.cGC."
+
+bass += "c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f. "
+bass += "c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f. "
+bass += "c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f. "
+bass += "c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.f.c.c.d#,e,G.A#.A#.f.g. "
+bass += "c-C-c-A#-a#-A#-a#A#.a#A#.a#A#.a#A#.a-A-a-f-f-f-f.f.f.f. "
+bass += "c-C-c-A#-a#-A#-a#A#.a#A#.a#A#.a#A#.a-A-a-f-f-f-f.f.f.f. "
+bass += "cC.cC."
