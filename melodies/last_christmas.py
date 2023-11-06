a6080cb28cb0bd47c41424b3064e5e9988aa42c2
Exandreal <A.Kristensson@live.dk>
2023-11-06 22:10:52 +0100

    almost finished last_christmas

--- melodies/last_christmas.py
+++ melodies/last_christmas.py
@@ -16,9 +16,6 @@ basedrum = ""  # Stone
 flute = ""  # Clay / Honeycomb
 cow_bell = ""  # Soul sand
 
-xylophone += "3f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#."
-snare += "3-f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+"
-basedrum += "3f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+"
 
 #---------------- intro sequence ----------------#
 
@@ -32,6 +29,10 @@ guitar += "bf#*bf#*"
 guitar += "eB*eB*"
 guitar += "ae*ae*"
 
+xylophone += "3f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#."
+snare += "3-f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#-"
+basedrum += "3f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+"
+
 #---------------- chorus ----------------#
 
 pling += "e-.e-d-a.e.e.f#.d-.b.b."
@@ -43,3 +44,71 @@ guitar += "dA+d+dA+d+"
 guitar += "bf#+b+bf#+b+"
 guitar += "eB+e+eB+e+"
 guitar += "ae+a+ae+a+"
+
+xylophone += "3f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#."
+snare += "3-f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#-"
+basedrum += "3f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+"
+
+#---------------- middle ----------------#
+
+pling += "a--A-.a.--a.b.c#.d."
+pling += "e-d-c#----A.f#.d.b."
+pling += "g*+B.G.e.c#."
+pling += "A*+G.f#.e-" # everything except e is one octave higher than should be, because e could not go low enough...
+
+guitar += "dD++dD++"
+guitar += "bB++bB++"
+guitar += "eE++eE++"
+guitar += "aA++aA++"
+
+xylophone += "3f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#."
+snare += "3-f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#-"
+basedrum += "3f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+"
+
+#---------------- middle 2 ----------------#
+
+pling += "-e-e.d-d.A-.f#-e,d,-"
+pling += ".e.e.e.e.d-e.B.B-f#-e-d."
+pling += "-f#.b,d;e-.f#.f#.f#.b,d;e-."
+pling += ".d-d.c#.c#-c#.d.c#.c#.b-a-."
+
+guitar += "dA++dA++"
+guitar += "bf#++bf#++"
+guitar += "eB++eB++"
+guitar += "ae++ae++"
+
+xylophone += "3f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#."
+snare += "3-f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#-"
+basedrum += "3f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+"
+
+#---------------- middle 3 ----------------#
+
+pling += "+-.d.c#.d.c#.d.c#.d-."
+pling += "e-d.c#-d.e.c#-d.c#.d.c#-d-"
+pling += "f#.e.d.e-e.d.e.f#.e.d.e-d.c#.d."
+pling += "d-c#.d-c#.d.c#.d.c#.d.a--."
+
+guitar += "dA++dA++"
+guitar += "bf#++bf#++"
+guitar += "eB++eB++"
+guitar += "ae++ae++"
+
+xylophone += "3f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#."
+snare += "3-f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#-"
+basedrum += "3f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+"
+
+#---------------- chorus ----------------#
+
+pling += "e-.e-d-a.e.e.f#.d-.b.b."
+pling += "e.e.f#-d-.b.c#.d.c#.b--."
+pling += "f#-.e--b.f#.G.f#.e--b."
+pling += "c#.d.c#.c#-d-c#..a---."
+
+guitar += "dA+d+dA+d+"
+guitar += "bf#+b+bf#+b+"
+guitar += "eB+e+eB+e+"
+guitar += "ae+a+ae++"
+
+xylophone += "3f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#.f#....."
+snare += "3-f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+-"
+basedrum += "3f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#+f#++"
